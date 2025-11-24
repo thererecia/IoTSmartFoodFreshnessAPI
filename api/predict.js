fd0ff27e195205067351c6c9ef385d2e84a9a7e3
whereisrere <there.zyy@gmail.com>
2025-11-24 07:22:48 +0700

    update api

--- api/predict.js
+++ api/predict.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     const item = req.body.item || "unknown";
-    const status = item.toLowerCase().includes("apple") ? "fresh" : "rotten";
+
+    // Buat random fresh/rotten
+    const status = Math.random() > 0.5 ? "fresh" : "rotten";
 
     res.status(200).json({ item, status });
   } else {
